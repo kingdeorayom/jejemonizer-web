bfc2e5ff2bc16349a5bef2d2c7476dc65964156b
Serking de Orayom <kingdeorayom@gmail.com>
2023-11-22 09:08:07 +0800

    Revert "update: welcome to jejemonizer"
    This reverts commit 5feb0d0bf5d64a287f70d64f41aacfdab185162a.

--- components/Welcome.js
+++ components/Welcome.js
@@ -3,12 +3,7 @@ import styles from '@/styles/Home.module.css'
 const Welcome = () => {
     return (
         <>
-            <h1 className={styles.welcome} onClick={(e) => {
-                e.preventDefault();
-                window.location.href = 'buysellee://articles';
-            }}
-            >
-                Welcome to Jejemonizer</h1>
+            <h1 className={styles.welcome}>Welcome to Jejemonizer</h1>
             <h2 className={styles.disclaimer}>I don&#39;t know how you plan to use this, but this is just for fun. Maybe for password generation?</h2>
         </>
     )
